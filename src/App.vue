c0d26764448e73807fa56d40775e565a6200fbd6
Matteo Pompei <datxizzan@gmail.com>
2022-02-05 23:06:27 +0100

    Gloobal SCSS creato

--- src/App.vue
+++ src/App.vue
@@ -14,33 +14,5 @@ export default {
 </script>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-
-  // Da decommentare a fine lavoro
-  // -webkit-user-select: none;
-  // -ms-user-select: none;
-  // user-select: none;
-
-  // Da decommentare in mental breakdown
-  // border: 2px solid red;
-}
-
-body {
-  border-top: 15px solid rgb(125, 0, 228);
-  background: #000;
-  font-family: Arial, Helvetica, sans-serif;
-  color: #fff;
-}
-
-#logo {
-  margin: 50px auto 50px auto;
-  padding: 0 20px;
-  width: fit-content;
-  font-size: 3rem;
-  letter-spacing: 5px;
-  // border-bottom: 3px solid #fff;
-}
+@import "./assets/style/global.scss";
 </style>
