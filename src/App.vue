316b64456b8a79b18ce3853ecff3fa323052dd72
Matteo Pompei <datxizzan@gmail.com>
2022-02-05 23:03:45 +0100

    Base abbozzata

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="logo">Massaro Runner 🏃</div>
+    <Game />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Game from "./components/Game.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Game },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  // Da decommentare a fine lavoro
+  // -webkit-user-select: none;
+  // -ms-user-select: none;
+  // user-select: none;
+
+  // Da decommentare in mental breakdown
+  // border: 2px solid red;
+}
+
+body {
+  border-top: 15px solid rgb(125, 0, 228);
+  background: #000;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+}
+
+#logo {
+  margin: 50px auto 50px auto;
+  padding: 0 20px;
+  width: fit-content;
+  font-size: 3rem;
+  letter-spacing: 5px;
+  // border-bottom: 3px solid #fff;
 }
 </style>
