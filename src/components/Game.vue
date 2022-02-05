c6a5048cbfbbbce77169d009e152637c5524d39c
Milan Stojko <milanmilance2000.ms@gmail.com>
2022-02-06 00:20:25 +0100

    floating

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="game">
-    <div class="play" @click="startGame()">Gioca!!</div>
-    <img src="../assets/img/car.gif" alt="" id="massaro" />
+    <!-- <div class="play" @click="startGame()">Gioca!!</div> -->
+    <img
+      src="../assets/img/car.gif"
+      alt=""
+      id="massaro"
+      :class="{ jump: jump, fall: fall }"
+      @click="massaroJump()"
+    />
     <div class="sky"></div>
     <div class="ground"></div>
   </div>
@@ -12,17 +18,32 @@ export default {
   name: "Game",
   data() {
     return {
-      gameStart: false,
+      gameStart: true,
       gamePause: false,
       poits: 0,
       gameSpeed: 15,
+      jump: false,
+      fall: false,
     };
   },
   methods: {
-    startGame() {
-      if (!this.gameStart) {
-        this.gameStart = true;
-        console.log(this.gameStart);
+    // startGame() {
+    //   if (!this.gameStart) {
+    //     this.gameStart = true;
+    //     console.log(this.gameStart);
+    //   }
+    // },
+    massaroJump() {
+      if (!this.jump) {
+        setTimeout(() => {
+          this.jump = true;
+          if (this.jump == true) {
+            setTimeout(() => {
+              this.fall = true;
+            }, 500);
+          }
+        }, 0);
+        this.jump = false;
       }
     },
   },
